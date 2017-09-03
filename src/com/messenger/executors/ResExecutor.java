a1fd723c3a4ee91d31c81c1bcd3006fb43c54522
MRtecno98 <montese.giorgio@gmail.com>
2017-09-03 15:10:20 +0200

    Adjusting Conventions in MsgExecutor

--- src/com/messenger/executors/ResExecutor.java
+++ src/com/messenger/executors/ResExecutor.java
@@ -16,24 +16,22 @@ public class ResExecutor implements CommandExecutor {
 	}
 	
 	public boolean onCommand(CommandSender sender , Command cmd , String label , String[] args) {
-		if(sender instanceof Player) {
-			Player p = (Player)sender;
-				if(args.length < 1) {
-					p.sendMessage(ChatColor.RED + instance.description.getCommands().get("r").get("tooltip").toString());
-					return false;
-				}
-				if(!(instance.responses.containsKey(p))) {
-					p.sendMessage(ChatColor.RED + "You don't have recent messages!");
-					return false;
-				}
-				Player t = instance.responses.get(p);
-				if(!(t.isValid())) {
-					p.sendMessage(ChatColor.RED + "Player is not Online!");
-					return false;
-				}
-				t.sendMessage(ChatColor.GOLD + "[" + ChatColor.GRAY + p.getName() + ChatColor.RESET + " --> " + ChatColor.RED + "You" + ChatColor.GOLD + "] " + args[0]);
-				return true;
+		if(!(sender instanceof Player)) return false;
+		Player player = (Player)sender;
+		if(args.length < 1) {
+			player.sendMessage(ChatColor.RED + instance.description.getCommands().get("r").get("tooltip").toString());
+			return false;
 		}
-		return false;
+		if(!(instance.responses.containsKey(player))) {
+			player.sendMessage(ChatColor.RED + "You don't have recent messages!");
+			return false;
+		}
+		Player target = instance.responses.get(player);
+		if(!(target.isValid())) {
+			player.sendMessage(ChatColor.RED + "Player is not Online!");
+			return false;
+		}
+		target.sendMessage(ChatColor.GOLD + "[" + ChatColor.GRAY + player.getName() + ChatColor.RESET + " --> " + ChatColor.RED + "You" + ChatColor.GOLD + "] " + args[0]);
+		return true;
 	}
 }
