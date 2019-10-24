6e0b7cab66f9c167867dc0645491a3fcdc2ebc75
Marco <marcodemartino23@gmail.com>
2019-10-24 21:53:20 +0200

    Converted the project to a maven project Added a configuration file for the messages Improved the code Fixed a bug when sending a message

--- src/main/java/com/messenger/executors/MsgExecutor.java
+++ src/main/java/com/messenger/executors/MsgExecutor.java
@@ -1,34 +1,48 @@
 package com.messenger.executors;
 
+import com.messenger.main.Messenger;
+import it.xquickglare.quicklib.utils.Message;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import com.messenger.main.Messenger;
+import java.util.Arrays;
 
 public class MsgExecutor implements CommandExecutor {
-	Messenger instance;
-	
+
+	private Messenger instance;
+
 	public MsgExecutor(Messenger instance) {
 		this.instance = instance;
 	}
-	
+
 	public boolean onCommand(CommandSender sender , Command cmd , String label , String[] args) {
 		if(!(sender instanceof Player)) return false;
 		Player player = (Player)sender;
+
 		if(args.length < 2) {
 			player.sendMessage(ChatColor.RED + instance.description.getCommands().get("msg").get("tooltip").toString());
 			return false;
 		}
+
 		if(!(instance.getServer().getPlayer(args[0]).isValid())) {
-			player.sendMessage(ChatColor.RED + "Player is not Online!");
+			player.sendMessage(Message.format(instance.configuration.getString("player_not_online"), '&'));
 			return false;
 		}
+
 		Player target = instance.getServer().getPlayer(args[0]);
-		target.sendMessage(ChatColor.GOLD + "[" + ChatColor.GRAY + player.getName() + ChatColor.RESET + " --> " + ChatColor.RED + "You" + ChatColor.GOLD + "] " + args[1]);
+
+		Message message = new Message(instance.configuration.getString("message_sent"))
+				.addPlaceholder("%player", args[0])
+				.addPlaceholder("%message", String.join(" ",Arrays.copyOfRange(args, 1, args.length)))
+				.format('&');
+
+		message.send(target);
+
 		instance.responses.put(player, target);
+
 		return true;
 	}
 }
